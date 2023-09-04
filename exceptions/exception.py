edd1ae6e38f63c8b170e8dc9f0403cb491ed7988
Morten Tšinakov <morten.tshinakov@gmail.com>
2023-09-04 16:06:28 +0300

    Tests for model class + validation functions

--- exceptions/exception.py
+++ exceptions/exception.py
@@ -2,3 +2,15 @@ class IncompatibleLayerError(Exception):
     def __init__(self, message="Layers are incompatible"):
         self.message = message
         super().__init__(self.message)
+
+
+class DisconnectedLayersError(Exception):
+    def __init__(self, message="No connection between layers") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class ShapeMismatchError(Exception):
+    def __init__(self, message="Shape mismatched") -> None:
+        self.message = message
+        super().__init__(self.message)
